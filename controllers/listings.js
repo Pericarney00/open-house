098e04c227c0dcb7798e824b8ab4d76c9a8a71c2
pericarney00 <pericarney@gmail.com>
2025-07-27 15:29:46 -0500

    completed new.ejs and form. the route is complete

--- controllers/listings.js
+++ controllers/listings.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const Listing = require("../models/listing")
 
-
+// GET /listings index
 router.get("/", async (req, res) => {
   try {
     const allListings = await Listing.find({});
@@ -15,9 +15,16 @@ router.get("/", async (req, res) => {
   }
 });
 
+// GET /listings/new
+router.get("/new", (req, res) => {
+  try {
+    res.render("listings/new.ejs")
+  } catch (error) {
+    console.log(error)
+    res.redirect("/")
+  }
+})
 
 
-
-
-
+// Exports
 module.exports = router;
